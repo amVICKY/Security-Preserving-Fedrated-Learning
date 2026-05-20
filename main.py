3dd3daa6e5a89cd90aa64a507a05056b829a0e68
amVICKY <vikrantbansode0106@gmail.com>
2026-05-20 17:11:09 +0530

    Implemented configuration system and dataset

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Federated learning project initialized")
+from utils.config import load_config
+from data.dataset import get_dataloaders
+
+def main():
+
+    config = load_config()
+
+    print("Configuration Loaded Successfully")
+    print(config)
+
+    train_loader, test_loader = get_dataloaders(config)
+
+    print(f"Train batches: {len(train_loader)}")
+    print(f"Test batches: {len(test_loader)}")
+
+if __name__ == "__main__":
+    main()
+
+# print("Federated learning project initialized")
---
+++ utils/config.py
@@ -0,0 +1,12 @@
+import yaml
+
+configuration_path = r"D:\INTERNSHIP_PROJECT\secure_federated_learning\configs\config.yaml"
+
+def load_config(config_path=configuration_path):
+    with open(config_path,"r") as file:
+        config = yaml.safe_load(file)
+    
+    return config
+
+traing_dict = load_config()
+print(traing_dict["training"]["epochs"])
