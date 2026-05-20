8ac4d848350fb2ef2199de7c1892d67d4cff6b6b
amVICKY <vikrantbansode0106@gmail.com>
2026-05-20 18:10:38 +0530

    Simple CNN model Created

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+import torch
 from utils.config import load_config
 from data.dataset import get_dataloaders
+from models.cnn import CNN
 
 def main():
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     config = load_config()
-
-    print("Configuration Loaded Successfully")
-    print(config)
 
     train_loader, test_loader = get_dataloaders(config)
 
-    print(f"Train batches: {len(train_loader)}")
-    print(f"Test batches: {len(test_loader)}")
+    model = CNN(
+        num_classes=config["model"]["num_classes"]
+    ).to(device)
+    print(model)
+
+    sample_batch = next(iter(train_loader))
+    images,labels = sample_batch
+
+    images = images.to(device)
+    labels = labels.to(device)
+
+    outputs = model(images)
+
+    print(f"Device: {device}")
+    print(f"Input Shape: {images.shape}")
+    print(f"Ousstput Shape: {outputs.shape}")
+
 
 if __name__ == "__main__":
     main()
--- models/cnn.py
+++ models/cnn.py
@@ -1,2 +1,44 @@
-class CNN:
-    pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN(nn.Module):
+    
+    def __init__(self,num_classes=10):
+        super(CNN,self).__init__()
+        
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=32,
+            kernel_size=3,
+            padding=1
+        )
+
+        self.conv2 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3,
+            padding=1
+        )
+
+        self.pool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.fc1 = nn.Linear(64*7*7,128)
+        self.fc2 = nn.Linear(128,num_classes)
+
+    def __str__(self):
+        return "This is the simple Vision Model"
+
+    def forward(self,x):
+
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = x.view(x.size(0),-1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
